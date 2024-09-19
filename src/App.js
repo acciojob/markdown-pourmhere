3f2d27f64af1cea082d90c3a004d2df7d61583a5
POURMHERE <pourmhere@gmail.com>
2024-09-19 21:05:35 +0530

    ddd

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+// import MarkdownEditor from './markdownEditor';
+// import MarkdownPreview from './markdownPreview';
+// import "../styles/App.css"
+import './styles/App.css'
+import MarkdownEditor from './components/markdownEditor';
+import MarkdownPreview from './components/markdownPreview';
+
+const App = () => {
+  const [markdown, setMarkdown] = useState('');
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    // Simple markdown to HTML conversion
+    const convertedHtml = markdown
+      .replace(/^# (.*$)/gim, '<h1>$1</h1>') // Headers
+      .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+      .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+      .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>') // Bold
+      .replace(/\*(.*)\*/gim, '<em>$1</em>') // Italics
+      .replace(/\n/gim, '<br/>') // Line breaks
+      .replace(/\n\n/gim, '<p></p>'); // Paragraphs
+    
+    setHtml(convertedHtml);
+  }, [markdown]);
+
+  return (
+    <div className="app">
+      <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown}  />
+      <MarkdownPreview html={html} />
+    </div>
+  );
+};
+
+export default App;
